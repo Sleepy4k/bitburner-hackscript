4cb26fb5c649174702f84d9cbc6dc283ee7684c3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-02-12 02:08:06 +0700

    chore(script): change old execution script method to newest method

--- auto_purchase_server.js
+++ auto_purchase_server.js
@@ -1,13 +1,38 @@
+import { serverDomain, getRootedDomains } from "/helpers";
+
 /**
  * Main thread to exec hack script
  * @param {NS} ns provide main native hack function
  * @return void
  */
 export async function main(ns) {
-	const ram = 8.00;
+	/**
+	 * In this script i want to make automatic server purchase and upgrade it when we already have max server
+	 * With logic:
+	 * 1. Get total server that we can buy
+	 * 2. If we already reach max server that we can purchase then we jump to point 5
+	 * 3. If we still can purchase server, then we check if home server money equal or more than purchase cost
+	 * 4. After that, we copy script template and do some thread distribution, then exec that script
+	 * 5. When we reach the maximum number of server we can purchased, we make it upgrade system
+	 * 6. First we check if upgrade cost is less or equal to home server money
+	 * 7. If we can upgrade then do upgrade, also we do same thing like poin 4
+	 */
+
+	// Define base ram when we purchase server
+	const baseRAM = 8.00;
+
+	// Please don't change this, or script will throw an error (rare case)
+	// Because this game only support 25 total server
 	const maxServerPurchased = 25;
-	const baseServerHostName = "pserv";
+
+	// Define max ram that we can upgrade, if you want to make it more
+	// Feel free to change this value, remember ram format, 2, 4, 8, 16, ...
+	const totalMaxUpgradeRAM = 64;
+
+	// Passing argument from console or script to enable tail mode
 	const enableTail = ns.args[0] || false;
+
+  // Script template name, please provided file script name file
 	const scriptTemplateName = "nuke-template.js";
 
 	if (enableTail) ns.tail(ns.pid);
@@ -25,11 +50,11 @@ export async function main(ns) {
 		 */
 		if (totalServerCanBuy > 0 && purchasedServers.length < maxServerPurchased) {
 			// Check home domain money, if domain money equal to server cost then we make a purchase
-			if (ns.getServerMoneyAvailable("home") >= ns.getPurchasedServerCost(ram)) {
+			if (ns.getServerMoneyAvailable("home") >= ns.getPurchasedServerCost(baseRAM)) {
 				const serverIndex = purchasedServers.length + 1;
 
 				// Purchasing server and make unique domain with lastest server number
-				const server = ns.purchaseServer(baseServerHostName + "-" + serverIndex, ram);
+				const server = ns.purchaseServer(serverDomain + "-" + serverIndex, baseRAM);
 
 				// Hard copy template file to new server from home domain
 				ns.scp(scriptTemplateName, server, "home");
@@ -39,7 +64,7 @@ export async function main(ns) {
 
 				// Get current template script ram used
 				const totalScriptRAM = ns.getScriptRam(scriptTemplateName, server);
-
+				
 				// Get script max thread to run
 				const scriptThread = Math.floor(serverMaxRAM / totalScriptRAM);
 
@@ -64,22 +89,45 @@ export async function main(ns) {
 				// 	}
 				// }
 
-				const domain = (serverIndex < 12) ? "n00dles" : "foodnstuff";
+				// Get domains that already have root access
+				const rootedDomains = await getRootedDomains(ns);
 
-				// Run script in new server domain with current configuration
-				ns.exec(scriptTemplateName, server, scriptThread, scriptThread, false, domain);
+				// Get each domain thread
+				let eachDomainThread = Math.round(scriptThread / rootedDomains.length);
+
+				// Make an iterator for our loop
+				let iterator = 0;
+
+				for (let t = 0; t < scriptThread; t++) {
+					// Here some explain, when index (t) is less or equal than each domain thread
+					if (t <= eachDomainThread) {
+						// And if index (t) is even then we get current domain and execute script template
+						if (t % 2 == 0) {
+							const domain = rootedDomains[iterator];
+
+							ns.exec(scriptTemplateName, server, 2, 2, false, domain);
+						}
+
+						// Please that a note, that in last condition we check index (t) value
+						// If index (t) we increase one is more that each domain thread then we increase iterator value
+						// And each domain value is multipled by 2
+						if ((t + 1) > eachDomainThread) {
+							iterator++;
+							eachDomainThread *= 2;
+						}
+					}
+				}
 			}
 		} else if (purchasedServers.length > 1) {
 			// Loop all purchased server and check if we can upgrade some server ram? maybe
 			for (let i = 0; i < purchasedServers.length; i++) {
 				const server = purchasedServers[i];
-				const serverIndex = i + 1;
 
 				// Get current server max ram
 				let currentServerRam = ns.getServerMaxRam(server);
 
 				// Basically it's same as above, just we change some logic such as, purchase server and kill all running script
-				if (ns.getServerMoneyAvailable("home") >= ns.getPurchasedServerUpgradeCost(server, currentServerRam * 2) && currentServerRam <= 32) {
+				if (ns.getServerMoneyAvailable("home") >= ns.getPurchasedServerUpgradeCost(server, currentServerRam * 2) && currentServerRam < totalMaxUpgradeRAM) {
 					// Upgrade current server ram
 					ns.upgradePurchasedServer(server, currentServerRam * 2);
 
@@ -125,10 +173,34 @@ export async function main(ns) {
 					// 	}
 					// }
 
-					const domain = (serverIndex < 12) ? "n00dles" : "foodnstuff";
+					// Get domains that already have root access
+					const rootedDomains = await getRootedDomains(ns);
 
-					// Run script in new server domain with current configuration
-					ns.exec(scriptTemplateName, server, scriptThread, scriptThread, false, domain);
+					// Get each domain thread
+					let eachDomainThread = Math.round(scriptThread / rootedDomains.length);
+
+					// Make an iterator for our loop
+					let iterator = 0;
+
+					for (let t = 0; t < scriptThread; t++) {
+						// Here some explain, when index (t) is less or equal than each domain thread
+						if (t <= eachDomainThread) {
+							// And if index (t) is even then we get current domain and execute script template
+							if (t % 2 == 0) {
+								const domain = rootedDomains[iterator];
+
+								ns.exec(scriptTemplateName, server, 2, 2, false, domain);
+							}
+
+							// Please that a note, that in last condition we check index (t) value
+							// If index (t) we increase one is more that each domain thread then we increase iterator value
+							// And each domain value is multipled by 2
+							if ((t + 1) > eachDomainThread) {
+								iterator++;
+								eachDomainThread *= 2;
+							}
+						}
+					}
 				}	
 			}
 		}
