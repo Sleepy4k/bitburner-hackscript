fbc7a66c8b448290d8b1ea4d6cc386f2cf7cda62
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-02-12 02:10:53 +0700

    chore(script): make it more configurable and cleaner

--- stop_all_running_script.js
+++ stop_all_running_script.js
@@ -4,12 +4,39 @@
  * @return void
  */
 export async function main(ns) {
+  /**
+   * In this script, i want to make automatic kill running script
+   * With logic:
+   * 1. Get all purchased server
+   * 2. Check if current script is running on that server
+   * 3. If running then we stop that script
+   */
+
+  const stopAllScript = false;
+  const scriptName = "nuke-template.js";
+
   ns.tail(ns.pid);
 
   const servers = ns.getPurchasedServers();
 
+  ns.print(`Found ${servers.length} total purchased server`);
+
   for (let i = 0; i < servers.length; i++) {
     const server = servers[i];
-    ns.killall(server);
+
+    const isScriptRunning = ns.scriptRunning(scriptName, server);
+
+    if (isScriptRunning) {
+      ns.print(`Stopping script ${scriptName} on server ${server}`);
+
+      if (stopAllScript) ns.killall(server);
+      else ns.scriptKill(scriptName, server);
+    }
   }
+
+  ns.print("Job finished");
+
+  await ns.sleep(5000);
+
+  ns.closeTail(ns.pid);
 }
