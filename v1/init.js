b78e2b4f1454d829779c0b132720de07d0695d11
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-02-19 12:03:36 +0700

    chore(script): add flag option for init script

--- v1/init.js
+++ v1/init.js
@@ -2,18 +2,44 @@ import { execScript } from "./auto_exec_script";
 import { stopScript } from "./stop_all_running_script";
 import { getNukedDomains, scriptTemplateName } from "./helpers";
 
+const options = [
+  ["help", false],
+  ["enableTail", false],
+  ["disableHomeExec", true]
+];
+
 /**
  * Main thread to exec hack script
  * @param {NS} ns provide main native hack function
  * @return void
  */
 export async function main(ns) {
-	const rootedDomains = getNukedDomains(ns);
+  ns.disableLog("sleep");
+
+  const flags = ns.flags(options);
+
+  if (flags.help) {
+    ns.tprint("Run default auto script file and exec nuke template for home server");
+    ns.tprint("To run this script you can use flag like this : ");
+    ns.tprint("run init.js --help --enableTail --disableHomeExec");
+    ns.exit();
+  }
+
+  if (flags.enableTail) ns.tail(ns.pid);
 
   stopScript(ns, "home", null, true);
 
   ns.exec("auto_upgrade_hacknet.js", "home", 1);
   ns.exec("auto_purchase_server.js", "home", 1);
 
-  execScript(ns, "home", scriptTemplateName, rootedDomains);
+  if (flags.disableHomeExec) {
+    const rootedDomains = getNukedDomains(ns);
+
+    execScript(ns, "home", scriptTemplateName, rootedDomains);
+  }
+
+  if (flags.enableTail) {
+    await ns.sleep(5000);
+    ns.closeTail(ns.pid);
+  }
 }
